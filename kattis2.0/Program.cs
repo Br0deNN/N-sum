c76d72a54fb0d04531e81933fe72e9b9f70c3fb6
7R1XY <theo.broden@gmail.com>
2023-12-06 14:44:29 +0100

    Add project files.

---
+++ kattis2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Tim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            string y = Console.ReadLine();
+            string[] z = y.Split(' ');
+            int[] q = new int[x];
+            int sum = 0;
+
+            for (int i = 0; i < x; i++)
+            {
+                q[i] = int.Parse(z[i]);
+                sum += q[i];
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
